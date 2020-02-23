ecd2fe904475c6070e805a5a36e01a149b8aeb70
Jessie Conley <jconley2881@gmail.com>
2020-02-23 12:21:01 -0600

    added more photos to data file

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,8 @@
 const galleryItems = [
   {
     id: 1,
-    path: "images/goat_small.jpg",
-    description: "Photo of a goat taken at Glacier National Park.",
+    path: "images/cabin_sunset.jpg",
+    description: "Photo of cabin lake at sunset.",
     likes: 0
   },
   {
@@ -15,6 +15,37 @@ const galleryItems = [
     id: 3,
     path: "images/regan_cabin.jpg",
     description: "Photo of Regan at the cabin during summer 2019.",
+    likes: 0
+  },
+  {
+    id: 4,
+    path: "images/charlie_mike_newportbeach.jpg",
+    description: "Photo of Charlie and Mike at NewportBeach.",
+    likes: 0
+  },
+  {
+    id: 5,
+    path: "images/duckboats_boston.jpg",
+    description: "Family picture before duckboat tour.",
+    likes: 0
+  },
+
+  {
+    id: 6,
+    path: "images/family_cabin.jpg",
+    description: "Family photo before swimming at cabin summer 2019.",
+    likes: 0
+  },
+  {
+    id: 7,
+    path: "images/lou_christmas.jpg",
+    description: "Photo of Lou on Christmas 2019.",
+    likes: 0
+  },
+  {
+    id: 8,
+    path: "images/regan_newportbeach.jpg",
+    description: "Photo of Regan in the sand at NewportBeach.",
     likes: 0
   }
 ];
